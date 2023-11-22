c528d6e3a492432e15956c13513ed5d6cdd29a72
Nevacat <nevacattery217@naver.com>
2023-11-22 12:26:29 +0900

    Start:Nextjs 설치 및 초기 세팅

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/reset.css';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@/styles/theme';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import { Swiper } from 'swiper/react';
+
+export default function Home() {
+  return (
+    <Swiper>
+      <div>안녕하세요 인간입니다.</div>
+    </Swiper>
+  );
+}
