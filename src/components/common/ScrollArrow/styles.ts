7c464405e5da9ea306c8d385c1b60d98f2f0e1e0
Nevacat <nevacattery217@naver.com>
2023-11-23 17:34:05 +0900

    feat: 유도 화살표 컴포넌트화

---
+++ src/components/common/ScrollArrow/styles.ts
@@ -0,0 +1,26 @@
+import { keyframes } from '@emotion/css';
+import styled from '@emotion/styled';
+
+// 화살표 애니메이션
+const updown = keyframes`
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-10%);
+    }
+    100% {
+      transform: translateY(0);
+    }
+`;
+
+export const ArrowWrap = styled.div`
+  width: 10%;
+  position: absolute;
+  bottom: 2%;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  animation: ${updown} 1s ease infinite;
+  z-index: 2;
+`;
