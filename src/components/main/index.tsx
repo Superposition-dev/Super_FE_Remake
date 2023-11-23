4fb538b067344a9046b8cc265e7319e31ee7169b
Nevacat <nevacattery217@naver.com>
2023-11-23 17:34:22 +0900

    feat: 전시회 포스터 슬라이드 추가

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,6 +4,8 @@ import 'swiper/css/pagination';
 import { Mousewheel, Pagination } from 'swiper/modules';
 import * as S from './styles';
 import Intro from './Intro';
+import Poster from './Poster';
+import ScrollArrow from '../common/ScrollArrow';
 function MainPage() {
   return (
     <S.SwiperWrap
@@ -19,6 +21,10 @@ function MainPage() {
       <S.SwiperItem>
         <Intro />
       </S.SwiperItem>
+      <S.SwiperItem>
+        <Poster />
+      </S.SwiperItem>
+      <ScrollArrow />
     </S.SwiperWrap>
   );
 }
