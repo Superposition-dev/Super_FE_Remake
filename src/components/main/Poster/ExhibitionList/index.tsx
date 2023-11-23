1e1d5291aed502b1416d34290badc6c22a42e36d
Nevacat <nevacattery217@naver.com>
2023-11-23 17:33:15 +0900

    feat: 전시회 포스터 소개 페이지 구축 ( 진행중 )

---
+++ src/components/main/Poster/ExhibitionList/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './styles';
+function Exhibition() {
+  return <S.ExhibitionListWrap></S.ExhibitionListWrap>;
+}
+
+export default Exhibition;
---
+++ src/components/main/Poster/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import * as S from './styles';
+import Image from 'next/image';
+function Poster() {
+  return (
+    <S.PosterSection>
+      <S.TitleWrap>
+        <S.Title>Exhibition</S.Title>
+        <S.SubTitle>우주 속 다채로운 예술과의 조우</S.SubTitle>
+      </S.TitleWrap>
+      <S.PosterInfoWrap>
+        <S.PosterImage>
+          <Image src="/images/poster1.webp" alt="포스터" width={240} height={340} layout="responsive" />
+        </S.PosterImage>
+        <S.PosterTitleWrap>
+          <S.PosterTitle>성수 POSITION</S.PosterTitle>
+          <S.PosterSubWrap>
+            <S.PosterSubTitle>슈퍼포지션의 두 번째 기획 전시</S.PosterSubTitle>
+            <S.More>더보기</S.More>
+          </S.PosterSubWrap>
+        </S.PosterTitleWrap>
+        <S.PosterInfo>
+          {'연남에서의 첫 번째 만남. 이번엔 성수를 찾아갑니다!\n두 가지 버전으로 펼쳐질 성수전을 기대해주세요!'}
+        </S.PosterInfo>
+      </S.PosterInfoWrap>
+      <S.PosterSlideWrap></S.PosterSlideWrap>
+    </S.PosterSection>
+  );
+}
+
+export default Poster;
