dc3e3a55c94440821defffab1cc7a9653eb50df7
Nevacat <nevacattery217@naver.com>
2023-11-23 10:14:05 +0900

    Design: 인트로 페이지 애니메이션 추가

--- src/components/main/Intro/styles.ts
+++ src/components/main/Intro/styles.ts
@@ -1,20 +1,73 @@
 import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
-import { SwiperSlide } from 'swiper/react';
 
-const updown = keyframes`
+// 대형행성 애니메이션
+const keyframesPlanet = keyframes`
   0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-10%);
+    transform: rotate(55deg);
   }
   100% {
-    transform: translateY(0);
+    transform: rotate(415deg);
   }
 `;
 
+// 중형행성 애니메이션
+const showPlanet = keyframes`
+  0% {
+    transform: rotate(80deg) translateY(-40%);
+  }
+  100% {
+    transform: rotate(0) translateY(0);
+  }
+`;
+
+// 로고 애니메이션
+const fadeInUp = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  } 
+`;
+
+// 고양이, 작은행성 애니메이션
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  } 
+`;
+
+// 텍스트 애니메이션
+const fadeInText = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  } 
+`;
+
+// 화살표 애니메이션
+const updown = keyframes`
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-10%);
+    }
+    100% {
+      transform: translateY(0);
+    }
+`;
+
 export const SwiperSection = styled.section`
+  width: 100%;
   height: 100%;
   position: relative;
   overflow: hidden;
@@ -22,16 +75,21 @@ export const SwiperSection = styled.section`
   }
 `;
 
-export const PlanetWrap = styled.div<{ height: number }>`
+export const PlanetWrap = styled.div`
   position: absolute;
   width: 200%;
   left: -50%;
   right: -50%;
   transform: rotate(55deg);
+  z-index: 1;
+  animation: ${keyframesPlanet} 480s linear infinite;
   @media (max-width: 768px) {
-    top: -17%;
+    top: -18%;
   }
   @media (max-width: 411px) {
+    top: -22%;
+  }
+  @media (max-width: 380px) {
     top: -30%;
   }
 `;
@@ -39,9 +97,13 @@ export const PlanetWrap = styled.div<{ height: number }>`
 export const LogoWrap = styled.div`
   position: absolute;
   top: 15%;
-  left: 50%;
-  right: 50%;
-  transform: translateX(-50%);
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  z-index: 2;
+  transform: translateY(-50%);
+  opacity: 0;
+  animation: ${fadeInUp} 0.7s ease-in-out 0.2s forwards;
   @media (max-width: 768px) {
     width: 15%;
   }
@@ -54,6 +116,9 @@ export const SmallPlanetWrap = styled.div`
   position: absolute;
   top: 10%;
   left: 10%;
+  z-index: 2;
+  opacity: 0;
+  animation: ${fadeIn} 0.6s ease-in 1s forwards;
   @media (max-width: 768px) {
     width: 15%;
   }
@@ -62,6 +127,9 @@ export const MediumPlanetWrap = styled.div`
   position: absolute;
   top: 15%;
   right: -35%;
+  z-index: 2;
+  transform: rotate(80deg) translateY(-40%);
+  animation: ${showPlanet} 0.8s ease-in 0.2s forwards;
   @media (max-width: 768px) {
     width: 80%;
   }
@@ -69,6 +137,9 @@ export const MediumPlanetWrap = styled.div`
 
 export const CatWrap = styled.div`
   position: absolute;
+  z-index: 2;
+  opacity: 0;
+  animation: ${fadeIn} 1s ease-in-out 0.5s forwards;
   @media (max-width: 768px) {
     width: 30%;
     top: 46%;
@@ -85,6 +156,10 @@ export const TextWrap = styled.div`
   position: absolute;
   top: 30%;
   left: 5%;
+  z-index: 2;
+  opacity: 0;
+  transform: translateX(-10%);
+  animation: ${fadeInText} 1s ease-in-out 0.6s forwards;
   @media (max-width: 768px) {
     width: 80%;
     top: auto;
@@ -109,4 +184,13 @@ export const ArrowWrap = styled.div`
   right: 0;
   margin: 0 auto;
   animation: ${updown} 1s ease infinite;
+`;
+
+export const Gradient = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 1;
+  background-image: linear-gradient(140deg, rgba(0, 0, 0, 0.9) 3%, transparent 20%, transparent 90%, #101010 100%);
+  background-color: transparent;
 `;
