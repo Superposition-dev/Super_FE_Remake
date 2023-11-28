3a805e73a25baa207fb931b6d848d12125088e57
Nevacat <nevacattery217@naver.com>
2023-11-28 11:10:48 +0900

    feat: 첫번째 About 슬라이드 퍼블리싱 구현

---
+++ src/components/main/FirstAbout/styles.ts
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+export const AboutSection = styled.section`
+  overflow: hidden;
+  padding: 6rem 0;
+`;
+
+export const TitleWrap = styled.div`
+  text-align: center;
+`;
+
+export const Title = styled.h2`
+  ${({ theme }) => theme.font.title};
+`;
+
+export const SubTitle = styled.h3`
+  ${({ theme }) => theme.font.medium_14};
+`;
+
+export const ImageSwiperWrap = styled(Swiper)`
+  margin: 1vh 0;
+`;
+
+export const ImageSwiper = styled(SwiperSlide)`
+  position: relative;
+  img {
+    width: 100%;
+    height: 40%;
+  }
+`;
+
+export const ContentWrap = styled.div`
+  padding: 0 1rem;
+  margin-bottom: 2rem;
+`;
+
+export const ContentTitle = styled.h3`
+  ${({ theme }) => theme.font.bold_3vh};
+  margin-bottom: 1rem;
+`;
+
+export const ContentDesc = styled.p`
+  width: 80%;
+  color: ${({ theme }) => theme.colors.gray};
+  ${({ theme }) => theme.font.regular_14};
+  word-break: keep-all;
+`;
