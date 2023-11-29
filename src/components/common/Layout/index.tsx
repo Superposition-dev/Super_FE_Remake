86cda9c1e3e8d46c781329c28717bd7b7b99d931
Nevacat <nevacattery217@naver.com>
2023-11-29 17:27:23 +0900

    fix: path에 따른 레이아웃 height값 지정
    masonry구조로 인해 높이가 제대로 맞지 않는 현상

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import * as S from './styles';
+import { useRouter } from 'next/router';
 function Layout({ children, height }: { children: React.ReactNode; height: number }) {
+  const router = useRouter();
+  const { pathname } = router;
   return (
-    <S.Layout height={height}>
+    <S.Layout path={pathname} height={height}>
       <S.Background />
       <S.Cover />
       {children}
