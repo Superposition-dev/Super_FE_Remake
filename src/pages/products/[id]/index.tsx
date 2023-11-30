304f2da09f9c6d7e19798711e3b8081da76a6013
Nevacat <nevacattery217@naver.com>
2023-11-30 17:01:59 +0900

    feat:상품 상세 라우터 정의

---
+++ src/pages/products/[id]/index.tsx
@@ -0,0 +1,8 @@
+import ProductDetail from '@/components/productsDetail';
+import React from 'react';
+
+function ProductDetailPage() {
+  return <ProductDetail />;
+}
+
+export default ProductDetailPage;
