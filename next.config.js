cd6a79a78b1dc97b2ef963be33c1f06ebeeb1191
Nevacat <nevacattery217@naver.com>
2023-12-01 17:26:29 +0900

    fixed: 허용 이미지 주소 재설정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'i.pinimg.com' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'kr.object.ncloudstorage.com' },
+      { protocol: 'https', hostname: 'i.pinimg.com' },
+    ],
   },
 };
 
