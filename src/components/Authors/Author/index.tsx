73b1297874bf471ed45c9a69a373a7a5459238ec
Nevacat <nevacattery217@naver.com>
2023-12-03 19:36:29 +0900

    feat: 작가 데이터 API 연결

--- src/components/Authors/Author/index.tsx
+++ src/components/Authors/Author/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import * as S from './styles';
-function Author() {
+import { AuthorsProps } from '@/interface/authors';
+
+
+function Author({data}:{data:AuthorsProps}) {
+  const {name,profile,introduce,display} = data;
+
+
   return (
     <S.AuthorWrap>
       <S.AuthorImageWrap>
         <S.AuthorImage
-          src={'https://i.pinimg.com/474x/75/dd/74/75dd7445d292b84013f1b790d2eaaf0b.jpg'}
+          src={`https://kr.object.ncloudstorage.com/superposition-bucket/${profile}`}
           alt="작가"
           fill
         />
         <S.Border />
       </S.AuthorImageWrap>
-      <S.AuthorName>김민수</S.AuthorName>
-      <S.AuthorInfo>김민수는 1990년 1월 1일에 태어났습니다.</S.AuthorInfo>
-      <S.NowBox>NOW</S.NowBox>
+      <S.AuthorName>{name}</S.AuthorName>
+      <S.AuthorInfo>{introduce}</S.AuthorInfo>
+      {
+        display && <S.NowBox>NOW</S.NowBox>
+      }
     </S.AuthorWrap>
   );
 }
--- src/components/Authors/index.tsx
+++ src/components/Authors/index.tsx
@@ -3,26 +3,37 @@ import * as S from './styles';
 import CommonTitle from '../common/Title';
 import Search from '../common/Search';
 import Author from './Author';
+import { QueryClient, dehydrate, useQuery } from 'react-query';
+import { getAuthors } from '@/api/authors';
+import { AuthorsProps } from '@/interface/authors';
 
 const titleData = {
   title: '파트너 작가',
   subTitle: '슈퍼포지션의 작가들을 소개합니다.',
 };
 
+export const getStaticProps = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery('authors', getAuthors);
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
 function Authors() {
+  const { data } = useQuery('authors', getAuthors);
   return (
     <S.AuthorsContainer>
       <CommonTitle data={titleData} />
       <Search />
       <S.AuthorsWrap>
-        <Author />
-        <Author />
-        <Author />
-        <Author />
-        <Author />
-        <Author />
-        <Author />
-        <Author />
+        {
+          data?.map((author:AuthorsProps) => {
+            return <Author key={author.instagramId} data={author} />;
+          })
+        }
       </S.AuthorsWrap>
     </S.AuthorsContainer>
   );
