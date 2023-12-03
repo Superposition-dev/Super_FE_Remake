942b7a4834ab8d2d9fdfabe4d10b613ae389597e
Nevacat <nevacattery217@naver.com>
2023-12-03 19:58:14 +0900

    feat: 작가 페이지 상세 API 연결

--- src/components/productsDetail/index.tsx
+++ src/components/productsDetail/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import * as S from './styles';
 import Image from 'next/image';
+import { ProductProps } from '@/interface/product';
 
-const NOTE =
-  'Nisl faucibus sollicitudin elementum commodo cursus ullamcorper senectus ut. Urna euismod feugiat convallis in mi neque. Nascetur etiam blandit sem amet. Odio viverra molestie ';
+function ProductDetail({data}:{data:ProductProps}) {
+  const {picture,tags,title,artist,productNum,pictureInfo,price,description} = data;
 
-function ProductDetail() {
   return (
     <S.DetailContainer>
       <S.ImageWrap>
         <S.ProductImage
-          src="https://i.pinimg.com/474x/c6/a3/f2/c6a3f2bbe8537fed334798f0c55876ec.jpg"
+          src={`https://kr.object.ncloudstorage.com/superposition-bucket/${picture}`}
           alt="이미지"
           width={360}
           height={594}
@@ -20,26 +20,28 @@ function ProductDetail() {
       <S.ProductInfoWrap>
         <S.Copy>
           <S.CopyText>무단 도용 및 재배포를 금지합니다.</S.CopyText>
-          <S.CopyText>ⓒ 2023. 김에지. All rights reserved.</S.CopyText>
+          <S.CopyText>ⓒ 2023. {artist}. All rights reserved.</S.CopyText>
         </S.Copy>
         <S.FlexBox>
           <S.TitleWrap>
-            <S.Title>작품명</S.Title>
-            <S.Code>작품 코드</S.Code>
+            <S.Title>{title}</S.Title>
+            <S.Code>{productNum}</S.Code>
           </S.TitleWrap>
           <S.IsLike>
             <S.UnHeart />
           </S.IsLike>
         </S.FlexBox>
         <S.InfoWrap>
-          <S.InfoType>캔버스에 유화, </S.InfoType>
-          <S.InfoType>73*73cm, </S.InfoType>
-          <S.InfoType>2023</S.InfoType>
+          <S.InfoType>{pictureInfo.type}, </S.InfoType>
+          <S.InfoType>{pictureInfo.size}, </S.InfoType>
+          <S.InfoType>{pictureInfo.year}</S.InfoType>
         </S.InfoWrap>
         <S.Tags>
-          <S.Tag>#태그</S.Tag>
-          <S.Tag>#태그</S.Tag>
-          <S.Tag>#태그</S.Tag>
+          {
+            tags.map((item:string,index:number) => (
+              <S.Tag key={index}>#{item}</S.Tag>
+            ))
+          }
         </S.Tags>
         <S.AuthorWrap>
           <S.AuthorBox>
@@ -52,17 +54,17 @@ function ProductDetail() {
                 layout="responsive"
               />
             </S.AuthorImageWrap>
-            <S.AuthorName>작가명</S.AuthorName>
+            <S.AuthorName>{artist}</S.AuthorName>
           </S.AuthorBox>
           <S.RightArrow />
         </S.AuthorWrap>
         <S.AuthorNote>
           <S.AuthorTitle>작가노트</S.AuthorTitle>
-          <S.NoteText>{NOTE}</S.NoteText>
+          <S.NoteText>{description}</S.NoteText>
         </S.AuthorNote>
       </S.ProductInfoWrap>
       <S.PriceBox>
-        <S.Price>100,000원</S.Price>
+        <S.Price>{price}원</S.Price>
         <S.BuyButton>구매하기</S.BuyButton>
       </S.PriceBox>
     </S.DetailContainer>
