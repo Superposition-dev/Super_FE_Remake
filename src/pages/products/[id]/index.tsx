8773d6febf6565dff98bab0f186c2f840fd3a91b
Nevacat <nevacattery217@naver.com>
2023-12-03 19:59:16 +0900

    feat: 작가 상세 SSG 설정

--- src/pages/products/[id]/index.tsx
+++ src/pages/products/[id]/index.tsx
@@ -1,8 +1,32 @@
+import { getProduct, getProducts } from '@/api/products';
 import ProductDetail from '@/components/productsDetail';
+import { ProductProps } from '@/interface/product';
 import React from 'react';
 
-function ProductDetailPage() {
-  return <ProductDetail />;
+export async function getStaticPaths(){
+  const data = await getProducts();
+  const paths = data.map((product:ProductProps) => {
+    return {
+      params: { id: product.productId.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  }; 
+}
+
+export async function getStaticProps({ params }:any){
+  const data = await getProduct(params.id);
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+function ProductDetailPage({ data }:any) {
+  return <ProductDetail data={data} />;
 }
 
 export default ProductDetailPage;
