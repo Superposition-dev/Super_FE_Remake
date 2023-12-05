56951de5d2d68a5f66a30350225784ffa6fe28fb
Nevacat <nevacattery217@naver.com>
2023-12-05 17:28:43 +0900

    feat: 검색 텍스트 제거

--- src/components/common/Search/styles.ts
+++ src/components/common/Search/styles.ts
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
 import { IoSearch } from 'react-icons/io5';
+import { AiFillCloseCircle } from 'react-icons/ai';
 export const SearchWrap = styled.form`
   width: 100%;
   height: 3.4rem;
   position: relative;
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors.white};
 `;
 
 export const SearchInput = styled.input`
@@ -14,18 +17,23 @@ export const SearchInput = styled.input`
   padding: 0 1rem 0 3.5rem;
   ${({ theme }) => theme.font.regular_14};
   outline: none;
-  background-color: ${({ theme }) => theme.colors.dark_gray};
   color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.dark_gray};
   &::placeholder {
     color: ${({ theme }) => theme.colors.gray};
   }
 `;
 
 export const SearchIcon = styled(IoSearch)`
-  font-size: 1.6rem;
   position: absolute;
   top: 50%;
   left: 1rem;
   transform: translateY(-50%);
-  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const DeleteIcon = styled(AiFillCloseCircle)`
+  position: absolute;
+  top: 50%;
+  right: 1rem;
+  transform: translateY(-50%);
 `;
