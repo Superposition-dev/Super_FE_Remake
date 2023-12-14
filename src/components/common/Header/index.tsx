fa3f195adcafe76af4bd71f9ff0bc59887c90c2c
Nevacat <nevacattery217@naver.com>
2023-12-14 23:57:44 +0900

    refactor: 헤더 라우터 map으로 변경

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import * as S from './styles';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+
+const menuData = [
+  {
+    title: '전시 작품',
+    link: '/products',
+  },
+  {
+    title: '작가',
+    link: '/authors',
+  },
+]
+
 function Header() {
   const router = useRouter();
   const pathname = router.pathname;
@@ -12,8 +24,13 @@ function Header() {
         <Image src="/images/main_logo.webp" alt="로고" width={0} height={0} layout="responsive" />
       </S.LogoWrap>
       <S.NavWrap>
-        <S.NavItem href={'/products'}>전시 작품</S.NavItem>
-        <S.NavItem href={'/authors'}>작가</S.NavItem>
+        {
+          menuData.map((item, index) => (
+            <S.NavItem key={index} href={item.link} active={item.link === pathname?true:false}>
+              {item.title}
+            </S.NavItem>
+          ))
+        }
       </S.NavWrap>
     </S.HeaderWrap>
   );
