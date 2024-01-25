53dd17d622726f3832b41d6b6d53784bae176c98
aliyah521 <aliyah521@bitgarma.biz>
2024-01-25 14:05:06 +0900

    Style : 전시 목록 페이지 레이아웃 수정

--- src/components/Exhibition/List/styles.ts
+++ src/components/Exhibition/List/styles.ts
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const ExhibitionList = styled.section`
   ${({ theme }) => theme.flex.flexColumn};
   align-items: center;
+  gap: 20px;
   width: 100%;
   overflow: auto;
 `;
