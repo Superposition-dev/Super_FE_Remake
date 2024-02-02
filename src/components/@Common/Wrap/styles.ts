6f26c73a51d23e3fc8610185e36c788824572235
aliyah521 <aliyah521@bitgarma.biz>
2024-02-02 16:35:56 +0900

    Style : 스크롤 후 사이드바 열었을 때 위로 올라가 있는 버그 수정

--- src/components/@Common/Wrap/styles.ts
+++ src/components/@Common/Wrap/styles.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div<{ transparent: boolean }>`
   width: 100%;
+  height: 100%;
   padding: 5rem 2rem 2rem;
   background-color: ${({ theme, transparent }) => (transparent ? transparent : theme.colors.black)};
 `;
--- src/components/ExhibitionDetail/styles.ts
+++ src/components/ExhibitionDetail/styles.ts
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 
 export const ExhibitionDetailWrap = styled.div`
   width: 100%;
-  height: 100;
   display: flex;
   flex-direction: column;
   align-items: center;
