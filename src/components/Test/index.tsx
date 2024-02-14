1c7c057696772f3816399878a7a0964a19cdad67
Nevacat <nevacattery217@naver.com>
2024-02-14 22:46:21 +0900

    test: 리프레시 토큰 Samesite Test용 코드 작성

---
+++ src/components/Test/index.tsx
@@ -0,0 +1,39 @@
+import { getReToken, refreshTest } from '@/api/test'
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+
+const TestComponents = () => {
+
+  const [makeToken, setMakeToken] = useState(false)
+  const [reissueToken, setReissueToken] = useState(false)
+  const {data:token} = useQuery('token', refreshTest, {
+    enabled: makeToken,
+    onSuccess: () => {
+      setMakeToken(false)
+      console.log('토큰생성완료',token)
+    },
+    onError: (e) => {
+      setMakeToken(false)
+      console.log('토큰생성실패',e)
+    }
+  })
+  const {data:reToken} = useQuery('reToken', getReToken, {
+    enabled: reissueToken,
+    onSuccess: () => {
+      setReissueToken(false)
+      console.log('토큰재발급완료',reToken)
+    },
+    onError: (e) => {
+      setReissueToken(false)
+      console.log('토큰재발급실패',e)
+    }
+  })
+  return (
+    <div style={{position:'relative', top:'50%',left:'50%'}}>
+      <button style={{backgroundColor:'white',marginRight:'10px', cursor:'pointer'}} onClick={()=>setMakeToken(true)}>토큰생성</button>
+      <button style={{backgroundColor:'white',cursor:'pointer'}} onClick={()=>setReissueToken(true)}>토큰재발급</button>
+    </div>
+  )
+}
+
+export default TestComponents
