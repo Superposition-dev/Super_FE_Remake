72356759ce26364b50ba3771c466c4016efff03e
aliyah521 <aliyah521@bitgarma.biz>
2024-02-15 09:44:49 +0900

    Feat : 헤더 내 userInfo 요청 삭제 및 로그인 여부 상태 표시 처리

--- src/components/Mypage/index.tsx
+++ src/components/Mypage/index.tsx
@@ -5,11 +5,34 @@ import Profile from './User/Profile';
 import Taste from './User/Taste';
 import List from './User/List';
 import { getCookie } from '@/util/cookie';
+import { QueryClient, dehydrate, useQuery } from 'react-query';
+import { getMe } from '@/api/auth';
+import { UserType } from '@/interface/user';
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+  const token = getCookie('accessToken');
+  await queryClient.prefetchQuery('userInfo', () => getMe(token));
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
 
 function Mypage() {
   const [user, setUser] = useState<boolean>(true);
   const [author, setAuthor] = useState<boolean>(false);
   const token = getCookie('accessToken');
+
+  const { data: userInfo } = useQuery<UserType>(['userInfo'], () => getMe(token), {
+    initialData: () => {
+      const queryClient = new QueryClient();
+      return queryClient.getQueryData('userInfo');
+    },
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 60 * 24,
+  });
 
   const handleDivision = () => {
     setUser(!user);
@@ -31,7 +54,7 @@ function Mypage() {
         </S.MypageTopWrap>
         {user && (
           <>
-            <Profile />
+            <Profile userInfo={userInfo} />
             <Taste />
             <List />
           </>
