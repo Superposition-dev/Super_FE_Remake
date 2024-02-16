e07d72ac5b67c0dc014fbc00a3aa1491246e2340
aliyah521 <aliyah521@bitgarma.biz>
2024-02-16 16:56:23 +0900

    Fixed : CommonTitle 경로 변경

--- src/components/@Common/Title/index.tsx
+++ src/components/@Common/Title/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as S from './styles';
-import { CommonTitleProps } from '@/interface/commonTitle';
+import { CommonTitleProps } from '@/interface/common';
 
 function CommonTitle({ data }: { data: CommonTitleProps }) {
   const { TITLE, SUBTITLE } = data;
